8fbacc730b663a3c9fd6c8d252a7fda23e816a3e
Luis Iglesias <67150429+LuisIgl@users.noreply.github.com>
2024-04-09 00:24:49 +0200

    Update register.php

--- quizgame2/register.php
+++ quizgame2/register.php
@@ -1,43 +1,43 @@
 <?php
 
-  require "database.php";
+require "database.php";
 
-  $error = null;
+$error = null;
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["username"]) || empty($_POST["password"])) {
-      $error = "Please fill all the fields.";
+        $error = "Please fill all the fields.";
     } else {
-      $statement = $conn->prepare("SELECT * FROM account WHERE username = :username");
-      $statement->bindParam(":username", $_POST["username"]);
-      $statement->execute();
+        $statement = $conn->prepare("SELECT * FROM account WHERE username = :username");
+        $statement->bindParam(":username", $_POST["username"]);
+        $statement->execute();
 
-      if ($statement->rowCount() > 0) {
-        $error = "This username is taken.";
-      } else {
-        $conn
-          ->prepare("INSERT INTO account (username, password) VALUES (:username, :password)")
-          ->execute([
-            ":username" => $_POST["username"],
-            ":password" => password_hash($_POST["password"], PASSWORD_BCRYPT),
-          ]);
+        if ($statement->rowCount() > 0) {
+            $error = "This username is taken.";
+        } else {
+            $conn
+                ->prepare("INSERT INTO account (username, password) VALUES (:username, :password)")
+                ->execute([
+                    ":username" => $_POST["username"],
+                    ":password" => password_hash($_POST["password"], PASSWORD_BCRYPT),
+                ]);
 
-          $statement = $conn->prepare("SELECT * FROM account WHERE username = :username LIMIT 1");
-          $statement->bindParam(":username", $_POST["username"]);
-          $statement->execute();
-          $user = $statement->fetch(PDO::FETCH_ASSOC);
+            $statement = $conn->prepare("SELECT * FROM account WHERE username = :username LIMIT 1");
+            $statement->bindParam(":username", $_POST["username"]);
+            $statement->execute();
+            $user = $statement->fetch(PDO::FETCH_ASSOC);
 
-          session_start();
-          
-          unset($user["Password"]);
-          
-          $_SESSION["account"] = $user;
+            session_start();
 
-          header("Location: home.php");
+            unset($user["Password"]);
 
-      }
+            $_SESSION["account"] = $user;
+
+            header("Location: home.php");
+            exit();
+        }
     }
-  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -49,46 +49,46 @@
     <link rel="stylesheet" href="./static/css/register-style.css">
 </head>
 <body>
-    <?php
+<?php
 
-    require "database.php";
+require "database.php";
 
-    try {
-        $query = "SELECT Username, Punkte FROM Account ORDER BY Punkte DESC";
-        $statement = $conn->prepare($query);
-        $statement->execute();
+try {
+    $query = "SELECT Username, Punkte FROM Account ORDER BY Punkte DESC";
+    $statement = $conn->prepare($query);
+    $statement->execute();
 
-        $players = $statement->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        die("Datenbankfehler: " . $e->getMessage());
-    }
+    $players = $statement->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Datenbankfehler: " . $e->getMessage());
+}
 
-    ?>
-    <div class="container">
-        <div class="logo">
-            <img src="static/img/logo.png" alt="Logo">
-        </div>
-        <h1>Retro-Quiz</h1>
-        <form method="POST" action="register.php">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <button type="submit" class="register-button">Registrieren</button>
-        </form>
+?>
+<div class="container">
+    <div class="logo">
+        <img src="static/img/logo.png" alt="Logo">
     </div>
-    <?php if ($error): ?>
-        <div class="error-container">
-            <div class="error-message">
-                <p><?= $error ?></p>
-            </div>
+    <h1>Retro-Quiz</h1>
+    <form method="POST" action="register.php">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit" class="register-button">Registrieren</button>
+    </form>
+</div>
+<?php if ($error): ?>
+    <div class="error-container">
+        <div class="error-message">
+            <p><?= $error ?></p>
         </div>
-    <?php endif ?>
-    <div class="sidebar">
+    </div>
+<?php endif ?>
+<div class="sidebar">
     <h2>Rangliste</h2>
     <hr>
     <br>
     <?php foreach ($players as $player): ?>
         <p><?php echo htmlspecialchars($player['Username']); ?>: <?php echo $player['Punkte']; ?> Punkte</p>
     <?php endforeach; ?>
-    </div>
+</div>
 </body>
 </html>
