c6d21b5c1ef44d84061dd3be2592fa9fc5ba33ab
Luis Iglesias <67150429+LuisIgl@users.noreply.github.com>
2024-04-09 00:00:03 +0200

    Update login.php

--- quizgame2/login.php
+++ quizgame2/login.php
@@ -17,7 +17,7 @@
       } else {
         $account = $statement->fetch(PDO::FETCH_ASSOC);
 
-        if (isset($account["password"]) && !password_verify($_POST["password"], $account["password"])) {
+        if (isset($account["username"]) && !password_verify($_POST["password"], $account["password"])) {
           $error = "Invalid credentials.";
         } else {
           session_start();
@@ -39,136 +39,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Retro-Quiz - Login</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #EFCF5D;
-        }
-        * {
-            font-size: 24px;
-        }
-        .container {
-            position: relative;
-            text-align: center;
-            border-radius: 50px;
-            padding: 30px;
-            width: 500px;
-            background: white; 
-            box-shadow: 20px 20px 50px -20px rgba(0,0,0,0.5);
-            border: 2px solid rgba(0, 0, 0, 0.5);
-        }
-
-        .sidebar {
-            position: fixed;
-            right: 0;
-            top: 0;
-            padding: 30px;
-            padding-top: 10px;
-            width: 25%;
-            height: 100%;
-            font-family: "Cabinet Grotesk Variable", sans-serif;
-            background: rgba(255, 255, 255, 0.4);
-            z-index: 1; /* Unter dem Hauptcontainer */
-            text-align: center;
-        }
-        
-        h1 {
-            margin-bottom: 10px;
-            font-size: 50px;
-            font-family: "Cabinet Grotesk Variable", sans-serif; 
-            text-shadow: 4px 4px 8px rgba(0, 0, 0, 0.4);
-            -webkit-text-stroke: 2px black;
-        }
-
-        h2 {
-            margin-bottom: 10px;
-            font-size: 40px;
-            font-family: "Cabinet Grotesk Variable", sans-serif;
-        }
-
-        .login-button {
-            background-color: #00F562;
-            color: black;
-            border-top: 2px solid black;
-            border-bottom: 2px solid black;
-            border-left: 2px solid black;
-            border-right: 2px solid black;
-            border-radius: 30px;
-            padding: 10px 30px;
-            font-size: 25px;
-            font-family: "Cabinet Grotesk Variable", sans-serif;
-            cursor: pointer;
-            font-weight: bold;
-            text-decoration: none;
-            display: inline-block;
-            margin-top: 20px;
-        }
-        .login-button:hover {
-            background-color: darkred;
-        }
-        .logo {
-            position: absolute;
-            top: -120px;
-            left: 50%;
-            transform: translateX(-50%);
-            width: 200px;
-            height: 200px;
-            border-radius: 50%;
-        }
-        .logo img {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: 200px;
-            height: 200px;
-            object-fit: cover;
-        }
-        input[type="text"],
-        input[type="password"] {
-            width: 60%;
-            padding: 10px;
-            margin: auto;
-            margin-top: 20px;
-            border: 2px solid black; 
-            border-radius: 20px;
-            box-sizing: border-box;
-            font-size: 20px;
-            display: block;
-        }
-        input::placeholder {
-            font-size: 16px;
-        }
-        .error-container {
-            position: absolute;
-            bottom: 20%;
-            width: 100%;
-            display: flex;
-            justify-content: center;
-        }
-        .error-message {
-            background-color: 	 #ee4466;
-            border: 2px solid black;
-            float: left;
-            padding: 20px 30px;
-            top: 80%;
-            text-align: center;
-        }
-        .error-message p {
-            color: black;
-            font-family: "Cabinet Grotesk Variable", sans-serif;
-            font-size: 18px;
-            font-weight: bolder;
-            line-height: 20px;
-            text-shadow: 1px 1px rgba(250,250,250,.3);
-            margin: 0;
-        }
-    </style>
+    <link rel="stylesheet" href="./static/css/login-style.css">
 </head>
 <body>
     <?php
@@ -176,7 +47,7 @@
     require "database.php";
 
     try {
-        $query = "SELECT Username, Punkte FROM Account ORDER BY Punkte DESC";
+        $query = "SELECT username, Punkte FROM Account ORDER BY Punkte DESC";
         $statement = $conn->prepare($query);
         $statement->execute();
 
@@ -209,7 +80,7 @@
     <hr>
     <br>
     <?php foreach ($players as $player): ?>
-        <p><?php echo htmlspecialchars($player['Username']); ?>: <?php echo $player['Punkte']; ?> Punkte</p>
+        <p><?php echo htmlspecialchars($player['username']); ?>: <?php echo $player['Punkte']; ?> Punkte</p>
     <?php endforeach; ?>
     </div>
 </body>
